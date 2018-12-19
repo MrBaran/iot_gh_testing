70f5d7377c5c868405ed7aeab11e938575f68802
k2controls <kkelly@k2controls.net>
2018-12-19 17:09:04 -0500

    Update main.py

--- iot_gh_int_tests/main.py
+++ iot_gh_int_tests/main.py
@@ -1,7 +1,7 @@
 from integration_tests import IntegrationTests
 
 def main():
-    test_service = IntegrationTests(debug=True)
+    test_service = IntegrationTests()
     test_service.test_all()
 
 if __name__ == "__main__":
